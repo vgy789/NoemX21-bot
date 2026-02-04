8d9135f0247e3706785d4559d60f5a101a56369b
vgy789 <sanek15604@gmail.com>
2026-02-04 18:57:25 +0300

    feat(database): добавлены миграции и SQL-запросы для управления пользователями и их учетными данными

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.25.6
 
 require (
 	github.com/PaulSonOfLars/gotgbot/v2 v2.0.0-rc.33
+	github.com/avast/retry-go v3.0.0+incompatible
 	github.com/caarlos0/env/v11 v11.3.1
+	github.com/jackc/pgx/v5 v5.8.0
 	github.com/joho/godotenv v1.5.1
 	github.com/stretchr/testify v1.11.1
 	go.uber.org/mock v0.6.0
@@ -12,6 +14,13 @@ require (
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/rogpeppe/go-internal v1.10.0 // indirect
+	golang.org/x/sync v0.18.0 // indirect
+	golang.org/x/text v0.31.0 // indirect
 )
---
+++ internal/database/queries/queries.sql
@@ -0,0 +1,91 @@
+-- queries.sql
+
+-- name: GetStudentByS21Login :one
+SELECT * FROM students WHERE s21_login = $1;
+
+-- name: GetStudentByRocketChatId :one
+SELECT * FROM students WHERE rocketchat_id = $1;
+
+-- name: UpsertStudent :one
+INSERT INTO students (
+    s21_login, rocketchat_id, campus_id, coalition_id, status, 
+    timezone, alternative_contact, has_coffee_ban
+) VALUES (
+    $1, $2, $3, $4, $5, $6, $7, $8
+)
+ON CONFLICT (s21_login) DO UPDATE SET
+    rocketchat_id = EXCLUDED.rocketchat_id,
+    campus_id = EXCLUDED.campus_id,
+    coalition_id = EXCLUDED.coalition_id,
+    status = EXCLUDED.status,
+    timezone = EXCLUDED.timezone,
+    alternative_contact = EXCLUDED.alternative_contact,
+    has_coffee_ban = EXCLUDED.has_coffee_ban,
+    updated_at = CURRENT_TIMESTAMP
+RETURNING *;
+
+-- name: GetUserAccountByExternalId :one
+SELECT * FROM user_accounts 
+WHERE platform = $1 AND external_id = $2;
+
+-- name: CreateUserAccount :one
+INSERT INTO user_accounts (
+    student_id, platform, external_id, username, is_searchable, role
+) VALUES (
+    $1, $2, $3, $4, $5, $6
+)
+RETURNING *;
+
+-- name: GetUserBotSettings :one
+SELECT * FROM user_bot_settings 
+WHERE user_account_id = $1;
+
+-- name: UpsertUserBotSettings :one
+INSERT INTO user_bot_settings (
+    user_account_id, language_code, notifications_enabled, review_post_campus_ids
+) VALUES (
+    $1, $2, $3, $4
+)
+ON CONFLICT (user_account_id) DO UPDATE SET
+    language_code = EXCLUDED.language_code,
+    notifications_enabled = EXCLUDED.notifications_enabled,
+    review_post_campus_ids = EXCLUDED.review_post_campus_ids,
+    updated_at = CURRENT_TIMESTAMP
+RETURNING *;
+
+-- name: GetPlatformCredentials :one
+SELECT * FROM platform_credentials 
+WHERE student_id = $1;
+
+-- name: UpsertPlatformCredentials :exec
+INSERT INTO platform_credentials (
+    student_id, password_enc, password_nonce, 
+    access_token, access_expires_at, refresh_token_enc, 
+    refresh_nonce, refresh_expires_at
+) VALUES (
+    $1, $2, $3, $4, $5, $6, $7, $8
+)
+ON CONFLICT (student_id) DO UPDATE SET
+    password_enc = EXCLUDED.password_enc,
+    password_nonce = EXCLUDED.password_nonce,
+    access_token = EXCLUDED.access_token,
+    access_expires_at = EXCLUDED.access_expires_at,
+    refresh_token_enc = EXCLUDED.refresh_token_enc,
+    refresh_nonce = EXCLUDED.refresh_nonce,
+    refresh_expires_at = EXCLUDED.refresh_expires_at,
+    updated_at = CURRENT_TIMESTAMP;
+
+-- name: GetRocketChatCredentials :one
+SELECT * FROM rocketchat_credentials 
+WHERE student_id = $1;
+
+-- name: UpsertRocketChatCredentials :exec
+INSERT INTO rocketchat_credentials (
+    student_id, rc_token_enc, rc_nonce
+) VALUES (
+    $1, $2, $3
+)
+ON CONFLICT (student_id) DO UPDATE SET
+    rc_token_enc = EXCLUDED.rc_token_enc,
+    rc_nonce = EXCLUDED.rc_nonce,
+    updated_at = CURRENT_TIMESTAMP;
