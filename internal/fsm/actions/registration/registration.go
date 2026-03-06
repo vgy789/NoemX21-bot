96353e68bbd789e9367f97bee86768f142e14fc0
vgy789 <sanek15604@gmail.com>
2026-03-06 07:37:53 +0300

    feat(reg): добавлена кнопка назад при проверке OTP, упрощён ввод login, небольшие исправления текста

--- internal/fsm/actions/registration/registration.go
+++ internal/fsm/actions/registration/registration.go
@@ -157,7 +157,7 @@ func Register(
 
 	// Validate School21 user
 	registry.Register("validate_school21_user", func(ctx context.Context, userID int64, payload map[string]any) (string, map[string]any, error) {
-		login := payload["login"].(string)
+		login := strings.ToLower(strings.TrimSpace(payload["login"].(string)))
 		log.Debug("validating school21 user", "login", login)
 
 		// 1. Get bot's own token to use for verification
@@ -196,7 +196,7 @@ func Register(
 
 	// Find and verify RocketChat user and update ID in DB
 	registry.Register("find_and_verify_rocket_user", func(ctx context.Context, userID int64, payload map[string]any) (string, map[string]any, error) {
-		login := payload["login"].(string)
+		login := strings.ToLower(strings.TrimSpace(payload["login"].(string)))
 		updates := resetRegistrationFlags()
 
 		// 1. Check if account already registered/linked in our DB
